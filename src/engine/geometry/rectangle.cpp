7e3b5bea64eefe44243c2c1fbe30e931f487a7af
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-07-26 11:43:35 +0200

    Use new matrix/vector in geometry rectangle

--- src/engine/geometry/rectangle.cpp
+++ src/engine/geometry/rectangle.cpp
@@ -1,34 +1,38 @@
 #include "rectangle.hpp"
 
+#include <utility>
+
+#include "matrix.hpp"
+
 using namespace engine::geometry;
 
 Rectangle::Rectangle()
         : Rectangle(Transform()) {}
 
-Rectangle::Rectangle(const Transform& transform, float width, float height)
-        : _transform(transform),
+Rectangle::Rectangle(Transform transform, float width, float height)
+        : _transform(std::move(transform)),
           _width(width),
           _height(height) {
   update_corners();
 }
 
 // Accessors
-auto Rectangle::transform() const -> Transform { return this->_transform; }
-auto Rectangle::width() const -> float { return this->_width; }
-auto Rectangle::height() const -> float { return this->_height; }
+auto Rectangle::transform() const -> Transform { return _transform; }
+auto Rectangle::width() const -> float { return _width; }
+auto Rectangle::height() const -> float { return _height; }
 
-auto Rectangle::topleft() const -> glm::vec3 { return this->_topleft; }
-auto Rectangle::topright() const -> glm::vec3 { return this->_topright; }
-auto Rectangle::botleft() const -> glm::vec3 { return this->_botleft; }
-auto Rectangle::botright() const -> glm::vec3 { return this->_botright; }
+auto Rectangle::topleft() const -> Vector<3> { return _topleft; }
+auto Rectangle::topright() const -> Vector<3> { return _topright; }
+auto Rectangle::botleft() const -> Vector<3> { return _botleft; }
+auto Rectangle::botright() const -> Vector<3> { return _botright; }
 
 // Mutators
 void Rectangle::update_corners() {
-  glm::vec3 xoffset = (this->_width / 2.0F) * Transform::world_right;
-  glm::vec3 yoffset = (this->_height / 2.0F) * Transform::world_up;
+  Vector<3> xoffset = (_width / 2.0F) * Vector<3>(Transform::world_right);
+  Vector<3> yoffset = (_height / 2.0F) * Vector<3>(Transform::world_up);
 
-  this->_topleft  = this->_transform.matrix() * glm::vec4(+xoffset + yoffset, 1.0F);
-  this->_topright = this->_transform.matrix() * glm::vec4(-xoffset + yoffset, 1.0F);
-  this->_botleft  = this->_transform.matrix() * glm::vec4(+xoffset - yoffset, 1.0F);
-  this->_botright = this->_transform.matrix() * glm::vec4(-xoffset - yoffset, 1.0F);
+  _topleft  = Matrix<4>(_transform.matrix()) * Vector<4>(+xoffset + yoffset, 1.0F);
+  _topright = Matrix<4>(_transform.matrix()) * Vector<4>(-xoffset + yoffset, 1.0F);
+  _botleft  = Matrix<4>(_transform.matrix()) * Vector<4>(+xoffset - yoffset, 1.0F);
+  _botright = Matrix<4>(_transform.matrix()) * Vector<4>(-xoffset - yoffset, 1.0F);
 }
--- src/engine/geometry/rectangle.hpp
+++ src/engine/geometry/rectangle.hpp
@@ -1,36 +1,34 @@
 #pragma once
 
 #include "transform.hpp"
-
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
+#include "vector.hpp"
 
 namespace engine::geometry {
 
   class Rectangle {
   public:
     Rectangle();
-    Rectangle(const Transform& transform, float width = 1.0F, float height = 1.0F);
+    Rectangle(Transform transform, float width = 1.0F, float height = 1.0F);
 
     // Accessors
     [[nodiscard]] auto transform() const -> Transform;
     [[nodiscard]] auto width() const -> float;
     [[nodiscard]] auto height() const -> float;
 
-    [[nodiscard]] auto topleft() const -> glm::vec3;
-    [[nodiscard]] auto topright() const -> glm::vec3;
-    [[nodiscard]] auto botleft() const -> glm::vec3;
-    [[nodiscard]] auto botright() const -> glm::vec3;
+    [[nodiscard]] auto topleft() const -> Vector<3>;
+    [[nodiscard]] auto topright() const -> Vector<3>;
+    [[nodiscard]] auto botleft() const -> Vector<3>;
+    [[nodiscard]] auto botright() const -> Vector<3>;
 
   private:
     Transform _transform;
     float _width;
     float _height;
 
-    glm::vec3 _topleft{};
-    glm::vec3 _topright{};
-    glm::vec3 _botleft{};
-    glm::vec3 _botright{};
+    Vector<3> _topleft{};
+    Vector<3> _topright{};
+    Vector<3> _botleft{};
+    Vector<3> _botright{};
 
     // Mutators
     void update_corners();
