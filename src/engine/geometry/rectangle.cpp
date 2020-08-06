4a580937dd38f71991596403d2984ebdd132fbb2
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-08-06 18:54:57 +0200

    Add json conversion to rectangle

--- src/engine/geometry/rectangle.cpp
+++ src/engine/geometry/rectangle.cpp
@@ -13,6 +13,7 @@ Rectangle::Rectangle(Transform transform, float width, float height)
         : _transform(transform),
           _width(width),
           _height(height) {
+
   update_corners();
 }
 
@@ -21,18 +22,27 @@ auto Rectangle::transform() const -> Transform { return _transform; }
 auto Rectangle::width() const -> float { return _width; }
 auto Rectangle::height() const -> float { return _height; }
 
-auto Rectangle::topleft() const -> Vector<3> { return _topleft; }
-auto Rectangle::topright() const -> Vector<3> { return _topright; }
 auto Rectangle::botleft() const -> Vector<3> { return _botleft; }
 auto Rectangle::botright() const -> Vector<3> { return _botright; }
+auto Rectangle::topleft() const -> Vector<3> { return _topleft; }
+auto Rectangle::topright() const -> Vector<3> { return _topright; }
+
+auto Rectangle::to_json() const -> debug::JSON {
+  debug::JSON json;
+  json["botleft"]  = botleft().to_json();
+  json["botright"] = botright().to_json();
+  json["topleft"]  = topleft().to_json();
+  json["topright"] = topright().to_json();
+  return json;
+}
 
 // Mutators
 void Rectangle::update_corners() {
   Vector<3> xoffset = (_width / 2.0F) * Vector<3>(Transform::world_right);
   Vector<3> yoffset = (_height / 2.0F) * Vector<3>(Transform::world_up);
 
-  _topleft  = Matrix<4>(_transform.matrix()) * Vector<4>(+xoffset + yoffset, 1.0F);
-  _topright = Matrix<4>(_transform.matrix()) * Vector<4>(-xoffset + yoffset, 1.0F);
-  _botleft  = Matrix<4>(_transform.matrix()) * Vector<4>(+xoffset - yoffset, 1.0F);
-  _botright = Matrix<4>(_transform.matrix()) * Vector<4>(-xoffset - yoffset, 1.0F);
+  _botleft  = _transform.matrix() * Vector<4>(+xoffset - yoffset, 1.0F);
+  _botright = _transform.matrix() * Vector<4>(-xoffset - yoffset, 1.0F);
+  _topleft  = _transform.matrix() * Vector<4>(+xoffset + yoffset, 1.0F);
+  _topright = _transform.matrix() * Vector<4>(-xoffset + yoffset, 1.0F);
 }
--- src/engine/geometry/rectangle.hpp
+++ src/engine/geometry/rectangle.hpp
@@ -5,6 +5,8 @@
 
 namespace engine::geometry {
 
+  /// @todo Transform contains scale, so either remove width/height or don't take transform.
+  /// @todo Don't expose the transform like this, because update_corners ends up desynced.
   class Rectangle {
   public:
     Rectangle();
@@ -15,20 +17,22 @@ namespace engine::geometry {
     [[nodiscard]] auto width() const -> float;
     [[nodiscard]] auto height() const -> float;
 
-    [[nodiscard]] auto topleft() const -> Vector<3>;
-    [[nodiscard]] auto topright() const -> Vector<3>;
     [[nodiscard]] auto botleft() const -> Vector<3>;
     [[nodiscard]] auto botright() const -> Vector<3>;
+    [[nodiscard]] auto topleft() const -> Vector<3>;
+    [[nodiscard]] auto topright() const -> Vector<3>;
+
+    [[nodiscard]] auto to_json() const -> debug::JSON;
 
   private:
     Transform _transform;
     float _width;
     float _height;
 
-    Vector<3> _topleft{};
-    Vector<3> _topright{};
     Vector<3> _botleft{};
     Vector<3> _botright{};
+    Vector<3> _topleft{};
+    Vector<3> _topright{};
 
     // Mutators
     void update_corners();
