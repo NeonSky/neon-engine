d91dcd4db6b6217290e765140ef3711a7e6bbb0f
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-08-30 12:31:19 +0200

    Add api to scene

---
+++ src/engine/scene/scene.cpp
@@ -0,0 +1,5 @@
+#include "scene.hpp"
+
+using namespace engine::scene;
+
+IScene::IScene(SceneAPI& api) : _api(api) {}
--- src/engine/scene/scene.hpp
+++ src/engine/scene/scene.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
+#include "api.hpp"
+
 namespace engine::scene {
 
   /// @todo Consider only exposing update() to the user.
   class IScene : public graphics::IRenderable {
   public:
+    IScene(SceneAPI& api);
 
     virtual void update(float delta_time) = 0;
     virtual void render()                 = 0;
     virtual void gui()                    = 0;
 
   protected:
+    SceneAPI& _api;
   };
+
 }
