214181b250ca9dbaa0c434a3660238c969fdc817
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-08-30 12:05:34 +0200

    Move window and prepare multi-window support

---
+++ src/engine/os/window.cpp
@@ -0,0 +1,179 @@
+#include "window.hpp"
+#include "../debug/logger.hpp"
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_internal.h>
+#include <stdexcept>
+
+using namespace engine::os;
+
+Window::Window()
+        : Window(1280, 720) {}
+
+Window::Window(unsigned int width, unsigned int height)
+        : Window(width, height, "Application") {}
+
+Window::Window(unsigned int width, unsigned int height, const char* title)
+        : Window(width, height, title, nullptr) {}
+
+Window::Window(unsigned int width, unsigned int height, const char* title, Window& parent)
+        : Window(width, height, title, &parent) {}
+
+Window::Window(unsigned int width, unsigned int height, const char* title, Window* parent)
+        : _title(title),
+          _width(width),
+          _height(height),
+          _clear_color(0.2F, 0.2F, 0.2F, 1.0F) {
+
+  GLFWwindow* glfw_window_parent = nullptr;
+  if (parent)
+    glfw_window_parent = parent->_window.get();
+
+  _window = std::unique_ptr<GLFWwindow, std::function<void(GLFWwindow*)>>(
+                                                             glfwCreateWindow((int) width, (int) height, title, nullptr, glfw_window_parent),
+    [](GLFWwindow* window) {
+      glfwDestroyWindow(window);
+    });
+
+  if (_window == nullptr)
+    LOG_CRITICAL("Failed to create GLFW window.");
+
+  // Any callbacks are owned by `this`
+  glfwSetWindowUserPointer(_window.get(), reinterpret_cast<void*>(this));
+
+  glfwSetWindowCloseCallback(_window.get(), []([[maybe_unused]] GLFWwindow* window) {
+    auto* w = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+    for (auto& c : w->_on_should_close_callbacks)
+      c();
+  });
+
+  glfwSetKeyCallback(_window.get(), [](GLFWwindow* window, int key, [[maybe_unused]] int scancode, int action, int mods) {
+    auto* w = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    ActivatedModifiers modifiers;
+    modifiers.shift     = mods & 0x01;
+    modifiers.control   = mods & 0x02;
+    modifiers.alt       = mods & 0x04;
+    modifiers.super     = mods & 0x08;
+    modifiers.caps_lock = mods & 0x10;
+    modifiers.num_lock  = mods & 0x20;
+
+    for (auto& c : w->_on_key_callbacks)
+      c(key, static_cast<KeyAction>(action), modifiers);
+  });
+
+  glfwSetMouseButtonCallback(_window.get(), [](GLFWwindow* window, int button, int action, int mods) {
+    auto* w = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    ActivatedModifiers modifiers;
+    modifiers.shift     = mods & 0x01;
+    modifiers.control   = mods & 0x02;
+    modifiers.alt       = mods & 0x04;
+    modifiers.super     = mods & 0x08;
+    modifiers.caps_lock = mods & 0x10;
+    modifiers.num_lock  = mods & 0x20;
+
+    for (auto& c : w->_on_mouse_click_callbacks)
+      c(button, static_cast<MouseAction>(action), modifiers);
+  });
+
+  glfwSetCursorPosCallback(_window.get(), [](GLFWwindow* window, double xpos, double ypos) {
+    auto* w = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    for (auto& c : w->_on_mouse_move_callbacks)
+      c(xpos, ypos);
+  });
+
+  glfwSetScrollCallback(_window.get(), [](GLFWwindow* window, double xoffset, double yoffset) {
+    auto* w = reinterpret_cast<Window*>(glfwGetWindowUserPointer(window));
+
+    for (auto& c : w->_on_mouse_scroll_callbacks)
+      c(xoffset, yoffset);
+  });
+}
+
+void Window::lock_mouse_cursor() const {
+  glfwSetInputMode(_window.get(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+void Window::release_mouse_cursor() const {
+  glfwSetInputMode(_window.get(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+}
+
+void Window::hide_mouse_cursor() const {
+  glfwSetInputMode(_window.get(), GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+}
+
+void Window::set_as_current() {
+  glfwMakeContextCurrent(_window.get());
+}
+
+void Window::init_gui() {
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void) io; // FIXME
+  ImGui::StyleColorsDark();
+
+  std::string glsl_version = "#version 420";
+  ImGui_ImplGlfw_InitForOpenGL(_window.get(), true);
+  ImGui_ImplOpenGL3_Init(glsl_version.c_str());
+}
+
+void Window::update() {
+  glfwSwapBuffers(_window.get());
+  glfwPollEvents();
+}
+
+void Window::clear_screen() const {
+  glClearColor(_clear_color.r(), _clear_color.g(), _clear_color.b(), _clear_color.a());
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Window::toggle_fullscreen() {
+  GLFWmonitor* primary = glfwGetPrimaryMonitor();
+  glfwSetWindowMonitor(_window.get(), primary, 0, 0, 1920, 1080, 60);
+}
+
+void Window::on_should_close(const std::function<void()>& callback) const {
+  _on_should_close_callbacks.push_back(callback);
+}
+
+auto Window::title() const -> std::string {
+  return _title;
+}
+
+auto Window::is_closing() const -> bool {
+  return glfwWindowShouldClose(_window.get()) != 0;
+}
+
+auto Window::width() const -> unsigned int {
+  return _width;
+}
+
+auto Window::height() const -> unsigned int {
+  return _height;
+}
+
+auto Window::is_key_down(int keycode) const -> bool {
+  int status = glfwGetKey(_window.get(), keycode);
+  return (status == GLFW_PRESS || status == GLFW_REPEAT);
+}
+
+void Window::on_key(const std::function<void(KeyCode, KeyAction, const ActivatedModifiers&)>& callback) const {
+  _on_key_callbacks.push_back(callback);
+}
+
+void Window::on_mouse_click(const std::function<void(MouseCode, MouseAction, const ActivatedModifiers&)>& callback) const {
+  _on_mouse_click_callbacks.push_back(callback);
+}
+
+void Window::on_mouse_move(const std::function<void(float, float)>& callback) const {
+  _on_mouse_move_callbacks.push_back(callback);
+}
+
+void Window::on_mouse_scroll(const std::function<void(float, float)>& callback) const {
+  _on_mouse_scroll_callbacks.push_back(callback);
+}
---
+++ src/engine/os/window.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "../geometry/vector.hpp"
+#include "input.hpp"
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+#include <functional>
+#include <vector>
+
+namespace engine::os {
+
+  /// @todo title should be std::string
+  /// @todo clear_color should be graphics::color
+  class Window {
+  public:
+    Window();
+    Window(unsigned int width, unsigned int height);
+    Window(unsigned int width, unsigned int height, const char* title);
+    Window(unsigned int width, unsigned int height, const char* title, Window& parent);
+
+    // Mutators
+    void lock_mouse_cursor() const;
+    void release_mouse_cursor() const;
+    void hide_mouse_cursor() const;
+
+    void set_as_current();
+    void init_gui();
+    void update();
+    void clear_screen() const;
+    void toggle_fullscreen();
+
+    void on_should_close(const std::function<void()>& callback) const;
+
+    void on_key(const std::function<void(KeyCode, KeyAction, const ActivatedModifiers&)>& callback) const;
+    void on_mouse_click(const std::function<void(MouseCode, MouseAction, const ActivatedModifiers&)>& callback) const;
+    void on_mouse_move(const std::function<void(float, float)>& callback) const;
+    void on_mouse_scroll(const std::function<void(float, float)>& callback) const;
+
+    // Accessors
+    [[nodiscard]] auto title() const -> std::string;
+    [[nodiscard]] auto is_closing() const -> bool;
+    [[nodiscard]] auto width() const -> unsigned int;
+    [[nodiscard]] auto height() const -> unsigned int;
+
+    [[nodiscard]] auto is_key_down(KeyCode keycode) const -> bool;
+
+  private:
+    Window(unsigned int width, unsigned int height, const char* title, Window* parent);
+
+    std::unique_ptr<GLFWwindow, std::function<void(GLFWwindow*)>> _window;
+    std::string _title;
+    unsigned int _width  = 0;
+    unsigned int _height = 0;
+    geometry::Vector<4> _clear_color;
+
+    mutable std::vector<std::function<void()>> _on_should_close_callbacks;
+    mutable std::vector<std::function<void(KeyCode, KeyAction, const ActivatedModifiers&)>> _on_key_callbacks;
+    mutable std::vector<std::function<void(MouseCode, MouseAction, const ActivatedModifiers&)>> _on_mouse_click_callbacks;
+    mutable std::vector<std::function<void(float, float)>> _on_mouse_move_callbacks;
+    mutable std::vector<std::function<void(float, float)>> _on_mouse_scroll_callbacks;
+  };
+
+}
