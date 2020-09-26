9b77609b91b132a2fbd46921e28861ec699f6317
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2020-09-26 17:49:17 +0200

    Update rubik scene to new ecs style

--- src/app/scenes/rubik.cpp
+++ src/app/scenes/rubik.cpp
@@ -1,20 +1,90 @@
 #include "rubik.hpp"
 
+#include "../rubiks_cube.hpp"
+
+#include "../../engine/debug/debug_drawer.hpp"
 #include "../../engine/debug/logger.hpp"
+#include "../../engine/geometry/line_segment.hpp"
+#include "../../engine/graphics/component/cuboid.hpp"
+#include "../../engine/graphics/component/line_segment.hpp"
 
 using namespace app::scenes;
 using namespace engine;
 
-Rubik::Rubik(scene::SceneAPI& api) : IScene(api) {
+Rubik::Rubik(engine::scene::SceneAPI& api,
+             engine::scene::Node& root)
+        : _api(api),
+          _root(root),
+          _lines(_root.add_child()),
+          _cuboid1(_root.add_child()),
+          _cuboid2(_root.add_child()),
+          _cuboid3(_root.add_child()),
+          _rubiks_cube(_root.add_child()) {
 
-  _camera = std::make_unique<debug::DebugCamera>(_api.input_manager(), geometry::Rigidbody(geometry::Vector<3>(1.0F, 5.0F, -20.0F)));
+  _camera     = std::make_unique<debug::DebugCamera>(_api.input_manager(), geometry::Rigidbody(geometry::Vector<3>(1.0F, 5.0F, -20.0F)));
+  _api.camera = (graphics::Camera*) _camera.get();
 
-  _rubiks_cube = std::make_unique<RubiksCube>(_api.renderer(), geometry::Transform(geometry::Vector<3>(-4.5F, 2, 4.5F)));
-  _gltf_stone  = std::make_unique<graphics::GLTFModel>(_api.renderer(), "cube/Cube.gltf", geometry::Transform(geometry::Vector<3>(-1.0F, 1.0F, -1.0F)));
+  std::vector<graphics::component::LineSegment> line_segments;
 
-  _cuboid1 = std::make_unique<graphics::Cuboid>(_api.renderer(), geometry::Transform(geometry::Vector<3>(0.5F, 0.5F, 0.5F), geometry::Orientation(), geometry::Vector<3>(1.0F, 1.0F, 1.0F)), graphics::Color(0.6F, 1.0F, 1.0F));
-  _cuboid2 = std::make_unique<graphics::Cuboid>(_api.renderer(), geometry::Transform(geometry::Vector<3>(3.5F, 1.0F, 3.5F), geometry::Orientation(), geometry::Vector<3>(1.0F, 2.0F, 1.0F)), graphics::Color(0.6F, 1.0F, 0.6F));
-  _cuboid3 = std::make_unique<graphics::Cuboid>(_api.renderer(), geometry::Transform(geometry::Vector<3>(6.0F, 0.25F, 1.5F), geometry::Orientation(), geometry::Vector<3>(2.0F, 0.5F, 1.0F)), graphics::Color(1.0F, 0.6F, 0.6F));
+  // Draw grid
+  for (int x = -10; x <= 10; x++)
+    line_segments.push_back({
+
+      .geometry = geometry::LineSegment(geometry::Point<3>((float) x, 0, 10),
+                                        geometry::Point<3>((float) x, 0, -10)),
+      .width    = 2.0F,
+    });
+
+  for (int z = -10; z <= 10; z++)
+    line_segments.push_back({
+      .geometry = geometry::LineSegment(geometry::Point<3>(10, 0, (float) z),
+                                        geometry::Point<3>(-10, 0, (float) z)),
+      .width    = 2.0F,
+    });
+
+  // Draw coordinate axis
+  line_segments.push_back({
+    .geometry = geometry::LineSegment(geometry::Point<3>(),
+                                      geometry::Point<3>(10, 0, 0)),
+    .color    = graphics::Color(1.0F, 0.0F, 0.0F),
+    .width    = 5.0F,
+  });
+
+  line_segments.push_back({
+    .geometry = geometry::LineSegment(geometry::Point<3>(),
+                                      geometry::Point<3>(0, 10, 0)),
+    .color    = graphics::Color(0.0F, 1.0F, 0.0F),
+    .width    = 5.0F,
+  });
+  line_segments.push_back({
+    .geometry = geometry::LineSegment(geometry::Point<3>(),
+                                      geometry::Point<3>(0, 0, 10)),
+    .color    = graphics::Color(0.0F, 0.0F, 1.0F),
+    .width    = 5.0F,
+  });
+
+  _lines.add_component<graphics::component::LineSegments>(line_segments);
+
+  _cuboid1.add_component<geometry::Transform>(geometry::Vector<3>(0.5F, 0.5F, 0.5F),
+                                              geometry::Orientation(),
+                                              geometry::Vector<3>(1.0F, 1.0F, 1.0F));
+  _cuboid1.add_component<graphics::component::Cuboid>(geometry::Transform(),
+                                                      graphics::Color(0.6F, 1.0F, 1.0F));
+
+  _cuboid2.add_component<geometry::Transform>(geometry::Vector<3>(3.5F, 1.0F, 3.5F),
+                                              geometry::Orientation(),
+                                              geometry::Vector<3>(1.0F, 2.0F, 1.0F));
+  _cuboid2.add_component<graphics::component::Cuboid>(geometry::Transform(),
+                                                      graphics::Color(0.6F, 1.0F, 0.6F));
+
+  _cuboid3.add_component<geometry::Transform>(geometry::Vector<3>(6.0F, 0.25F, 1.5F),
+                                              geometry::Orientation(),
+                                              geometry::Vector<3>(2.0F, 0.5F, 1.0F));
+  _cuboid3.add_component<graphics::component::Cuboid>(geometry::Transform(),
+                                                      graphics::Color(1.0F, 0.6F, 0.6F));
+
+  _rubiks_cube.add_component<geometry::Transform>(geometry::Vector<3>(-4.5F, 2, 4.5F));
+  _rubiks_cube.add_component<RubiksCube>(_api.renderer());
 
   const auto& input = _api.input_manager();
   input.on_key([&]([[maybe_unused]] const os::Window& window,
@@ -22,54 +92,31 @@ Rubik::Rubik(scene::SceneAPI& api) : IScene(api) {
                    engine::os::KeyAction action,
                    const engine::os::ActivatedModifiers& mods) {
     if (action == os::KeyAction::PRESS) {
+      auto& cube = _rubiks_cube.component<RubiksCube>();
 
       if (keycode == GLFW_KEY_KP_4)
-        _rubiks_cube->rotate_left(mods.shift);
+        cube.rotate_left(mods.shift);
       if (keycode == GLFW_KEY_KP_6)
-        _rubiks_cube->rotate_right(mods.shift);
+        cube.rotate_right(mods.shift);
       if (keycode == GLFW_KEY_KP_2)
-        _rubiks_cube->rotate_bot(mods.shift);
+        cube.rotate_bot(mods.shift);
       if (keycode == GLFW_KEY_KP_8)
-        _rubiks_cube->rotate_top(mods.shift);
+        cube.rotate_top(mods.shift);
       if (keycode == GLFW_KEY_KP_5)
-        _rubiks_cube->rotate_back(mods.shift);
+        cube.rotate_back(mods.shift);
       if (keycode == GLFW_KEY_KP_0)
-        _rubiks_cube->rotate_front(mods.shift);
+        cube.rotate_front(mods.shift);
     }
   });
 }
 
 void Rubik::update(float delta_time) {
-  _time += delta_time;
+  static float time = 0.0F;
+  time += delta_time;
 
-  _rubiks_cube->transform().orientation() = geometry::Rotation(0, _time, 0);
+  _rubiks_cube.component<geometry::Transform>().orientation() = geometry::Rotation(0, time, 0);
 
-  _cuboid1->transform().orientation() = geometry::Rotation(0, 0, _time);
-  _cuboid2->transform().orientation() = geometry::Rotation(0, _time, 0);
-  _cuboid3->transform().orientation() = geometry::Rotation(_time, 0, 0);
-}
-
-void Rubik::render() {
-  geometry::Matrix<4> view_projection = _camera->projection_matrix() * _camera->view_matrix();
-
-  _dd.set_line_width(2.0F);
-  for (int x = -10; x <= 10; x++)
-    _dd.draw_line(geometry::Vector<3>((float) x, 0, 10), geometry::Vector<3>((float) x, 0, -10));
-  for (int z = -10; z <= 10; z++)
-    _dd.draw_line(geometry::Vector<3>(10, 0, (float) z), geometry::Vector<3>(-10, 0, (float) z));
-  _dd.render(view_projection);
-
-  _dd.set_line_width(5.0F);
-  _dd.draw_transform(geometry::Transform(geometry::Vector<3>(), geometry::Orientation(), geometry::Vector<3>(10.0F, 10.0F, 10.0F)));
-  _dd.render(view_projection);
-
-  _rubiks_cube->render(view_projection);
-  _gltf_stone->render(view_projection);
-
-  _cuboid1->render(view_projection, true);
-  _cuboid2->render(view_projection, true);
-  _cuboid3->render(view_projection, true);
-}
-
-void Rubik::gui() {
+  _cuboid1.component<geometry::Transform>().orientation() = geometry::Rotation(0, 0, time);
+  _cuboid2.component<geometry::Transform>().orientation() = geometry::Rotation(0, time, 0);
+  _cuboid3.component<geometry::Transform>().orientation() = geometry::Rotation(time, 0, 0);
 }
--- src/app/scenes/rubik.hpp
+++ src/app/scenes/rubik.hpp
@@ -1,32 +1,28 @@
 #pragma once
 
+#include "../../engine/scene/script.hpp"
+
 #include "../../engine/debug/debug_camera.hpp"
-#include "../../engine/debug/debug_drawer.hpp"
-#include "../../engine/graphics/gltf_model.hpp"
-#include "../../engine/scene/scene.hpp"
-#include "../rubiks_cube.hpp"
 
 namespace app::scenes {
 
-  class Rubik : public engine::scene::IScene {
+  class Rubik : public engine::scene::IScript {
   public:
-    Rubik(engine::scene::SceneAPI& api);
+    Rubik(engine::scene::SceneAPI& api, engine::scene::Node& root);
 
     void update(float delta_time) override;
-    void render() override;
-    void gui() override;
 
   private:
-    std::unique_ptr<engine::debug::DebugCamera> _camera;
-    std::unique_ptr<RubiksCube> _rubiks_cube;
-    std::unique_ptr<engine::graphics::GLTFModel> _gltf_stone;
-    std::unique_ptr<engine::graphics::Cuboid> _cuboid1;
-    std::unique_ptr<engine::graphics::Cuboid> _cuboid2;
-    std::unique_ptr<engine::graphics::Cuboid> _cuboid3;
-    std::unique_ptr<engine::graphics::Rectangle> _test;
+    engine::scene::SceneAPI& _api;
+    engine::scene::Node& _root;
 
-    engine::debug::DebugDrawer _dd;
-    float _time = 0.0F;
+    engine::scene::Node& _lines;
+    engine::scene::Node& _cuboid1;
+    engine::scene::Node& _cuboid2;
+    engine::scene::Node& _cuboid3;
+    engine::scene::Node& _rubiks_cube;
+
+    std::unique_ptr<engine::debug::DebugCamera> _camera;
   };
 
 }
